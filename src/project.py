96d9e1b1b32716af7974df825d5f289a4d6d8ff5
Michelle Noriega <mmn210003@utdallas.edu>
2023-12-03 02:15:24 -0600

    added movement and cat code

--- src/project.py
+++ src/project.py
@@ -2,13 +2,6 @@ import pygame
 import random
 from PIL import Image
 
-'''def generate_sprite(filename):
-    with (Image.open("exploring_space.png") as bg,
-          Image.open("duck sprite.png") as sprite):
-        x = 100
-        y = 100
-        bg.paste(sprite, (x, y), mask=sprite.getchannel('A'))
-        bg.show()'''
 class SpaceBackground(pygame.sprite.Sprite):
     def __init__ (self, image_file, location):
         pygame.sprite.Sprite.__init__(self)
@@ -17,28 +10,46 @@ class SpaceBackground(pygame.sprite.Sprite):
         self.rect.left, self.rect.top = location
 
 
-class Duck(pygame.sprite.Sprite):
-    def __init__(self, image_file, speed, location):
+class DuckSprite(pygame.sprite.Sprite):
+    def __init__(self, image_file, location):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load(image_file)
         self.rect = self.image.get_rect()
         self.rect.left, self.rect.top = location
+        self.move_x = 0
+        self.move_y = 0
+
+    '''def movement(self, x, y):
+        self.move_x += x
+        self.move_y +- y
+
+    def update(self):
+        self.rect.x = self.rect.x + self.move_x
+        self.rect.y = self.rect.y + self.move_y'''
+
+
         
 
 
 def main():
     pygame.init()
     pygame.display.set_caption("Exploring Space :D")
-    resolution = (960, 540)
+    resolution = (1280, 720)
     screen = pygame.display.set_mode(resolution)
-    move = 5
+    
+    x = 0
+    y = 0
+    width = 50
+    height = 50
+    move = 2
     Background = SpaceBackground('exploring_space.png', [0,0])
-    #sprite = generate_sprite(resolution)
+    duck = DuckSprite('duck sprite.png', [0,0])
     running = True
     while running:
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                exit()
+
         
         #movement mechanics
         keys = pygame.key.get_pressed()
@@ -52,11 +63,16 @@ def main():
             x += move
 
         # Render & Display
-        #lightBlue = pygame.Color(210, 226, 252)
-        #screen.fill(lightBlue)
-        #pygame.display.flip()
         screen.fill([0, 0, 0])
         screen.blit(Background.image, Background.rect)
+        #Cat Generation
+        pygame.font.init()
+        font = pygame.font.SysFont("Courier New", 30)
+        screen.blit(font.render("/\\_/\\", True, (255,255,255)), (10, 10))
+        screen.blit(font.render(">^w^<", True, (255,255,255)), (10, 50))
+        screen.blit(font.render("(___)", True, (255,255,255)), (10, 90))
+        screen.blit(duck.image, duck.rect)
+        pygame.draw.rect(screen, (255,0,0), (x, y, width, height))
         pygame.display.flip()
     pygame.quit()
 
